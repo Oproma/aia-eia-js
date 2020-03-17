a82d8e82ff15eecadaa398c2441b453cbb6b0491
Richard Belisle <rbelisle@oproma.com>
2020-03-17 15:07:18 -0400

    Added FontAwesome

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ import { StylesManager } from "survey-vue";
 import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
